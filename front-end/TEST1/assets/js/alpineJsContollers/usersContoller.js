65bc7092afe17fb7c1728b200b862b927d281495
hossein <roohollahazimii1361@gmail.com>
2023-07-28 16:58:10 +0330

    part21&22

--- front-end/TEST1/assets/js/alpineJsContollers/usersContoller.js
+++ front-end/TEST1/assets/js/alpineJsContollers/usersContoller.js
@@ -2,10 +2,12 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', () => ({
         users: [],
         isLoading: false,
+        showAddModal: false,
         getUsers() {
             this.isLoading = true
             axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                 this.users = res.data
+            }).finally(() => {
                 this.isLoading = false
             })
         }
